5c6fdb1428f6a587db15c187b62af5ddc67f160a
DanielH11 <123413380+DanielH11@users.noreply.github.com>
2025-02-05 16:50:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 import streamlit as st
+from streamlit.elements.heading import Divider
 import streamlit_authenticator as stauth
 import yaml
 from yaml.loader import SafeLoader
 import bcrypt
+import pandas as pd
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt 
+import seaborn as sn
 
 #ABRIR YAML, ARQUIVO CONTENDO OS REGISTROS DE USUÁRIOS
 with open('config.yaml') as file:
@@ -22,6 +28,12 @@ if "logged_in" not in st.session_state:
 if "sign_up" not in st.session_state:
     st.session_state.sign_up = False
 
+if "data" not in st.session_state:
+    st.session_state.data = np.array([])
+
+if "n_count" not in st.session_state:
+    st.session_state.n_count = 0
+
 #CONTAINER PARA LOGIN
 def login():
   #DIVIDIR TELA EM DOIS
@@ -39,12 +51,15 @@ def login():
     with open('config.yaml', 'r') as f:
       data = yaml.full_load(f)
     #BUSCA NO YAML UM REGISTRO COMPATÍVEL AO INSERIDO
-    for user in data['credentials']['usernames']:
-      #SE EMAIL E SENHA COINCIDEREM
-      if data['credentials']['usernames'][user]['email'] == email and bcrypt.checkpw(bytes(senha, "UTF-8"), bytes(data['credentials']['usernames'][user]['password'], "UTF-8")):
-        #USUÁRIO LOGADO E REINICIAR
-        st.session_state.logged_in = True
-        st.rerun()
+    try:
+      for user in data['credentials']['usernames']:
+        #SE EMAIL E SENHA COINCIDEREM
+        if data['credentials']['usernames'][user]['email'] == email and bcrypt.checkpw(bytes(senha, "UTF-8"), bytes(data['credentials']['usernames'][user]['password'], "UTF-8")):
+          #USUÁRIO LOGADO E REINICIAR
+          st.session_state.logged_in = True
+          st.rerun()
+    except:
+      st.error("Usuário e senha não batem")
   col2.header("Não tem conta ainda?")
   if col2.button("Cadastrar-se"):
     #LEVAR AO CADASTRO
@@ -83,10 +98,119 @@ def cadastro():
     st.session_state.sign_up= False;
     st.rerun()
 
+def weib(x,n,a):
+  return (a / n) * (x / n)**(a - 1) * np.exp(-(x / n)**a)
+
+def mean_rank(i):
+  return (i-0.3)/(st.session_state.n_count+0.4)
+
+def y(t):
+  aux = 1-mean_rank(t)
+  auxx = np.log(aux)
+  auxxx = np.log(-auxx)
+  #st.write(aux, auxx, auxxx)
+  #st.write("np.log(-np.log(1-mean_rank(t)): ", np.log(-np.log(1-mean_rank(t))))
+  return np.log(-np.log(1-mean_rank(t)))
+
+def soma_x(data):
+  soma=0;
+  for a in data:
+    x = np.log(a)
+    soma += x
+  return soma
+
+def soma_y(data):
+  soma=0;
+  for i in range(st.session_state.n_count):
+    soma += y(i+1)
+    #st.write("soma[",i+1,"]", y(i+1))
+  return soma
+
+def soma_xx(data):
+  soma=0;
+  for a in data:
+    x = np.log(a)
+    soma += x*x
+  return soma  
+
+def xpory(data):
+  i=1
+  soma=0;
+  for a in data:
+    x = np.log(a)
+    soma += x*y(i)
+    #st.write("x:", x, "y:", y(i), "soma:", soma)
+    i+=1
+  return soma
+
 #APLICAÇÃO PRINCIPAL
 def app():
-  st.write("APP") 
+  #Número de colunas
+  colunas = 5
 
+  st.header("Inserir manualmente dados de tempo de vida")
+  col_a, col_b = st.columns(2)
+
+  add_value = col_a.number_input("Adicionar tempo de vida", step=1.0, value=None,format="%f ", key="add_input")
+
+  if col_a.button("Adicionar"):
+    if not np.isnan(add_value):  # Ensure valid input
+      st.session_state.data = np.append(st.session_state.data, add_value)
+      st.session_state.data = np.sort(st.session_state.data)
+      st.success(f"Valor {add_value} adicionado!")
+
+  remove_value = col_a.number_input("Remover um tempo de vida", step=1.0, value=None, format="%f ", key="remove_input")
+
+  if col_a.button("Remover"):
+    if not np.isnan(remove_value):
+      matches = np.isclose(st.session_state.data, remove_value, atol=1e-9)
+      if np.any(matches):
+        first_match_idx = np.where(matches)[0][0]
+        st.session_state.data = np.delete(st.session_state.data, first_match_idx)
+        st.session_state.data = np.sort(st.session_state.data)
+        st.success(f"Valor {remove_value} removido!")
+      else:
+        st.warning(f"Valor {remove_value} não encontrado!")
+
+  st.header("Dados")
+  data = st.file_uploader("Carregar dataset. Atenção: ao carregá-lo, sobrescreverá os dados anteriores.")
+  if data:
+    try:
+      data = pd.read_csv(data, header=None).to_numpy().flatten()
+      data = np.sort(data)
+      st.session_state.data = data
+    except:
+      st.error("Não é um arquivo csv")
+  if st.button("Calcular a distribuição Weibull"):
+    data = st.session_state.data
+  
+    #st.write(data)
+    st.session_state.n_count = len(data)
+    n_count = st.session_state.n_count
+    a = (n_count*xpory(data)-soma_x(data)*soma_y(data))/(n_count*soma_xx(data)-soma_x(data)*soma_x(data))
+    b = (soma_xx(data)*soma_y(data)-soma_x(data)*xpory(data))/(n_count*soma_xx(data)-soma_x(data)*soma_x(data))
+    n = np.exp(-b / a)
+    beta = a
+    x = np.linspace(data.min(), data.max(), 1000)
+    weibull_d = weib(x, n, beta)
+
+    # Plot histogram and Weibull PDF
+    bin_size = (data.max()-data.min())/n_count
+    fig, ax = plt.subplots()
+    ax.hist(data, bins=16, density=True, alpha=0.5, label="Histograma")
+    ax.plot(x, weibull_d, label="Weibull")
+    ax.set_xlabel("Tempo de vida")
+    ax.set_ylabel("Densidade")
+    plt.grid(True)
+    st.pyplot(fig)
+
+    st.write("Beta: ", str(beta))
+    st.write("N: ", str(n))
+
+
+  chunks = [st.session_state.data[i:i+colunas] for i in range(0, len(st.session_state.data), colunas)]
+  df = pd.DataFrame(chunks).add_prefix("Coluna ")
+  col_b.dataframe(df, height=280, hide_index=True)
 
 #INÍCIO DO PROGRAMA E DIRECIONAMENTO
 if st.session_state.sign_up:
